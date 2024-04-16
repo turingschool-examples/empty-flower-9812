43300850bc1b23f5cdb8f56d7758d4acb63f4902
Mel Langhoff <mlanghoff@uwalumni.com>
2024-04-16 12:38:00 -0600

    Model tested and functioning

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -2,6 +2,9 @@ class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
     @rides = @mechanic.rides
+    @mechanic.add_ride(params[:ride_id])
+    
+    redirect_to mechanic_path(@mechanic)
   end
 
 end
--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,33 @@
 require "rails_helper"
 
 RSpec.describe Mechanic, type: :model do
+  before :each do
+    @six_flags = AmusementPark.create!(name: 'Six Flags', admission_cost: 75)
+    @universal = AmusementPark.create!(name: 'Universal Studios', admission_cost: 80)
+
+    @hurler = @six_flags.rides.create!(name: 'The Hurler', thrill_rating: 7, open: true)
+    @scrambler = @six_flags.rides.create!(name: 'The Scrambler', thrill_rating: 4, open: true)
+    @ferris = @six_flags.rides.create!(name: 'Ferris Wheel', thrill_rating: 7, open: false)
+
+    @jaws = @universal.rides.create!(name: 'Jaws', thrill_rating: 5, open: true)
+
+    @john = Mechanic.create!(name: 'John', years_experience: 5)
+    @joe = Mechanic.create!(name: 'Joe', years_experience: 8)
+
+    @john.rides << @hurler
+    @john.rides << @scrambler
+    @joe.rides << @ferris
+    @joe.rides << @jaws
+  end
+
   describe "relationships" do
     it { should have_many(:mechanic_rides) }
     it { should have_many(:rides).through(:mechanic_rides) }
+  end
+
+  it '#add_ride' do
+    @john.add_ride(@jaws.id)
+    
+    expect(@john.rides).to include(@jaws)
   end
 end
